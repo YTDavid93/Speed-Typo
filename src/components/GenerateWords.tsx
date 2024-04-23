212e6de83ed9cfc63f4c8a8c1e50b565260dea49
YTDavid93 <ytd3809@gmail.com>
2024-04-23 14:17:29 +0545

    seprate components to generate random words

---
+++ src/components/GenerateWords.tsx
@@ -0,0 +1,11 @@
+
+
+const GenerateWords = ({ words }: {words: string}) => {
+  return (
+    <div>
+     <h1 className=" text-4xl text-center text-slate-500">{words}</h1>
+    </div>
+  )
+}
+
+export default GenerateWords
