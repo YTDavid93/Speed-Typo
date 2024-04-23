92829d17589a8ee867fb8a26358f9eddd2a3c1d3
YTDavid93 <ytd3809@gmail.com>
2024-04-23 15:09:09 +0545

    add animation

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,17 +1,56 @@
+import { motion } from "framer-motion";
+
 interface ResultsProps {
   errors: number;
   accuracyPercentage: number;
   typed: number;
   className?: string;
 }
-const Results = ({ errors, accuracyPercentage, typed, className }: ResultsProps) => {
+const Results = ({
+  errors,
+  accuracyPercentage,
+  typed,
+  className,
+}: ResultsProps) => {
+  const initial = { opacity: 0 };
+  const animate = { opacity: 1 };
+  const duration = { duration: 0.3 };
+
   return (
-    <ul className={`flex flex-col items-center text-yellow-400 space-y-3 ${className}`}>
-      <li className=" text-xl font-semibold">Results</li>
-      <li>Accuracy: {accuracyPercentage}</li>
-      <li>Errors: {errors}</li>
-      <li>Typed: {typed}</li>
-    </ul>
+    <motion.ul
+      className={`flex flex-col items-center text-yellow-400 space-y-3 ${className}`}
+    >
+      <motion.li
+        initial={initial}
+        animate={animate}
+        className=" text-xl font-semibold"
+        transition={{ ...duration, delay: 0 }}
+      >
+        Results
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 0.5 }}
+      >
+        Accuracy: {accuracyPercentage}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 1 }}
+        className=" text-red-500"
+      >
+        Errors: {errors}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 1.4 }}
+      >
+        Typed: {typed}
+      </motion.li>
+    </motion.ul>
   );
 };
 
