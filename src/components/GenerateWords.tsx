e3f9261908e3ccc6d043a997158d764c2301707b
YTDavid93 <ytd3809@gmail.com>
2024-04-23 15:42:45 +0545

    remove previous styling

--- src/components/GenerateWords.tsx
+++ src/components/GenerateWords.tsx
@@ -3,7 +3,7 @@
 const GenerateWords = ({ words }: {words: string}) => {
   return (
     <div>
-     <h1 className=" text-4xl text-center text-slate-500">{words}</h1>
+     <h1 className="text-slate-500">{words}</h1>
     </div>
   )
 }
