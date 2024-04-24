a2de062a099275bfd2ee8f0363b7c67a29ce05b8
YTDavid93 <ytd3809@gmail.com>
2024-04-24 12:23:37 +0545

    remove stattic value from resutls passed it from useEngine hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
 import GenerateWords from "./components/GenerateWords";
 import CountdownTimer from "./components/CountdownTimer";
 import RestartButton from "./components/RestartButton";
-import { useRef } from "react";
 import Results from "./components/Results";
 import UserInputs from "./components/UserInputs";
 import WordsContainer from "./components/WordsContainer";
 import useEngine from "./hooks/useEngine";
+import { calculateAccuracyPercentage } from "./utils/helpers";
 
 function App() {
-  const { words, state, timeleft, typed } = useEngine();
-
-  const buttonRef = useRef<HTMLInputElement>(null);
+  const { words, state, timeleft, typed, restart, totalTyped, errors } =
+    useEngine();
 
   return (
     <>
       <CountdownTimer timeLeft={timeleft} />
       <WordsContainer>
         <GenerateWords words={words} />
-        <UserInputs className=" absolute inset-0" userInputs={typed} words={words} />
+        <UserInputs
+          className=" absolute inset-0"
+          userInputs={typed}
+          words={words}
+        />
       </WordsContainer>
       <RestartButton
         className={" mx-auto mt-10 text-slate-500"}
-        onRestart={() => buttonRef.current?.focus}
+        onRestart={restart}
       />
       <Results
+        state={state}
         className="mt-10"
-        errors={20}
-        accuracyPercentage={20}
-        typed={40}
+        errors={errors}
+        accuracyPercentage={calculateAccuracyPercentage(errors, totalTyped)}
+        typed={totalTyped}
       />
     </>
   );
