fe50e39eba024639d23a37c1f04b2465a20e01e5
YTDavid93 <ytd3809@gmail.com>
2024-04-24 10:38:41 +0545

    create function to calculate characters and accuracy percentage

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,26 @@
 
 export const formatPercentage = (percentage: number) => {
     return percentage.toFixed(0) + "%"
-;}
+};
+
+export const countErros = (actual: string, expected: string) => {
+  const expectedCharacters = expected.split(" ");
+
+  return expectedCharacters.reduce((errors, expectedchars, i) => {
+    const acutalChars = actual[i];
+    if (acutalChars !== expectedchars) {
+        errors++;
+    }
+    return errors;
+  }, 0);
+};
+
+
+export const calculateAccuracyPercentage = (errors: number, total: number) => {
+    if (total > 0) {
+        const correct = total - errors;
+        return (correct / total) * 100;
+    }
+
+    return 0;
+}
